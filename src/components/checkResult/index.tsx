1a346649f71158af9f08cc2478857beedc83dd77
Thiago Galvao <thiagosilva128@gmail.com>
2022-06-25 01:11:37 -0300

    fix: added button to notify a new bet and to clean screen

--- src/components/checkResult/index.tsx
+++ src/components/checkResult/index.tsx
@@ -1,15 +1,16 @@
-import { useContext, useState } from "react";
-import { ImFilesEmpty } from "react-icons//im";
-import { AiOutlineCheck } from "react-icons/ai";
-import { CgTrash } from "react-icons/cg";
-import { IoOptions } from "react-icons/io5";
-import { BetsContext } from "../../context/BetsContext";
-import { ModalContext } from "../../context/ModalContext";
-import { UserContext } from "../../context/UserContext";
-import { CardBets } from "../cardBets";
+import { useContext, useState } from "react"
+import { ImFilesEmpty } from "react-icons//im"
+import { AiOutlineCheck } from "react-icons/ai"
+import { CgTrash } from "react-icons/cg"
+import { IoOptions } from "react-icons/io5"
+import { BiPlus } from "react-icons/bi"
+import { BetsContext } from "../../context/BetsContext"
+import { ModalContext } from "../../context/ModalContext"
+import { UserContext } from "../../context/UserContext"
+import { CardBets } from "../cardBets"
 
 export function CheckResult() {
-  const { openModal } = useContext(ModalContext);
+  const { openModal } = useContext(ModalContext)
 
   const {
     allBets,
@@ -20,135 +21,157 @@ export function CheckResult() {
     updateBets,
     createAndSaveBets,
     getAllBetsHistory,
-  } = useContext(BetsContext);
+    blockInsertBets,
+    permitInsertBets,
+  } = useContext(BetsContext)
 
-  const { user } = useContext(UserContext);
+  const { user } = useContext(UserContext)
 
-  const [showButtonsDelete, setShowButtonsDelete] = useState<boolean>(false);
+  const [showButtonsDelete, setShowButtonsDelete] = useState<boolean>(false)
   const [showSingleButtonDelete, setShowSingleButtonDelete] =
-    useState<boolean>(false);
-  const [showAllBets, setShowAllBets] = useState<boolean>(false);
-  const [betRefUpdate, setBetRefUpdate] = useState<string>("");
-  const [canEdit, setCanEdit] = useState<boolean>(false);
-  const [showButtonEdit, setShowButtonEdit] = useState<boolean>(false);
+    useState<boolean>(false)
+  const [showAllBets, setShowAllBets] = useState<boolean>(false)
+  const [betRefUpdate, setBetRefUpdate] = useState<string>("")
+  const [canEdit, setCanEdit] = useState<boolean>(false)
+  const [showButtonEdit, setShowButtonEdit] = useState<boolean>(false)
+  const [isNewBet, setIsNewBet] = useState<boolean>()
 
-  const contentButton = showAllBets ? "voltar" : "meus jogos";
+  const contentButton = showAllBets ? "voltar" : "meus jogos"
 
-  const isEmpty = allBets.length === 0;
-  const TIME_CLOSE_AUTOMATICALLY = 60 * 1000 * 3; // 3 minutes
+  const isEmpty = allBets.length === 0
+  const TIME_CLOSE_AUTOMATICALLY = 60 * 1000 * 3 // 3 minutes
 
   const handleShowButtons = () => {
-    setShowButtonsDelete(true);
-    setShowSingleButtonDelete(true);
+    setShowButtonsDelete(true)
+    setShowSingleButtonDelete(true)
 
     setTimeout(() => {
-      setShowButtonsDelete(false);
-      setShowSingleButtonDelete(false);
-    }, TIME_CLOSE_AUTOMATICALLY);
-  };
+      setShowButtonsDelete(false)
+      setShowSingleButtonDelete(false)
+    }, TIME_CLOSE_AUTOMATICALLY)
+  }
 
   const hiddenButtons = () => {
-    setShowButtonsDelete(false);
-    setShowSingleButtonDelete(false);
-  };
+    setShowButtonsDelete(false)
+    setShowSingleButtonDelete(false)
+  }
 
   const sortArray = (arr: number[]) => {
-    const arrToSort: number[] = arr;
+    const arrToSort: number[] = arr
 
-    return arrToSort.sort((a: number, b: number) => a - b);
-  };
+    return arrToSort.sort((a: number, b: number) => a - b)
+  }
 
   const formatViewBet = (bet: number[]) => {
-    if (bet.length === 0) return;
+    if (bet.length === 0) return
 
-    const INITAL_STRING = 0;
-    const REMOVE_FROM = 3;
+    const INITAL_STRING = 0
+    const REMOVE_FROM = 3
 
-    const sortBet = sortArray(bet);
+    const sortBet = sortArray(bet)
 
     const formatedBet = sortBet.reduce((acc: string, number: number) => {
-      acc += `${String(number)} - `;
-      return acc;
-    }, "");
+      acc += `${String(number)} - `
+      return acc
+    }, "")
 
     const finalBet = formatedBet.substring(
       INITAL_STRING,
       formatedBet.length - REMOVE_FROM
-    );
-    return finalBet;
-  };
+    )
+    return finalBet
+  }
 
   const handleSaveBets = () => {
-    createAndSaveBets(allBets, user.id);
-    cleanAllBets();
-  };
+    createAndSaveBets(allBets, user.id)
+    cleanAllBets()
+  }
 
   const handleGetAllBetHistory = () => {
-    getAllBetsHistory(user.id);
-    setShowAllBets((oldState) => !oldState);
+    setShowAllBets((oldState) => !oldState)
+    setShowButtonEdit(true)
 
-    setCanEdit(false);
-    setShowButtonEdit(false);
-  };
+    if (!showAllBets) {
+      getAllBetsHistory(user.id)
+      setShowButtonEdit(false)
+    }
+    setCanEdit(false)
+  }
 
   const getBetAndShowOnScreen = (bets: string[], betRef: string) => {
-    replaceBetsWithNewOnes(bets);
-    setShowAllBets(false);
-    setBetRefUpdate(betRef);
+    replaceBetsWithNewOnes(bets)
+    setShowAllBets(false)
+    setBetRefUpdate(betRef)
 
-    setShowButtonEdit(true);
-  };
+    setIsNewBet(false)
+    setShowButtonEdit(true)
+    blockInsertBets()
+  }
 
   const permitEditBet = () => {
-    setCanEdit(true);
-  };
+    setCanEdit(true)
+    permitInsertBets()
+  }
 
   const handleUpdateBets = () => {
-    updateBets(allBets, betRefUpdate);
-  };
+    updateBets(allBets, betRefUpdate)
+  }
+
+  const cancelActionEdit = () => {
+    setCanEdit(false)
+    setShowButtonEdit(true)
+  }
+
+  const prepareToCreateNewBet = () => {
+    cleanAllBets()
+    permitInsertBets()
+    setCanEdit(false)
+    setShowButtonEdit(false)
+    setIsNewBet(true)
+  }
 
   return (
-    <div className='flex flex-col w-full h-96 bg-green-100'>
-      <h3 className='w-full text-center text-xs text-slate-500 border-b py-2 border-slave'>
+    <div className="flex flex-col w-full h-96 bg-green-100">
+      <h3 className="w-full text-center text-xs text-slate-500 border-b py-2 border-slave">
         confira seus jogos
       </h3>
       {showButtonsDelete ? (
-        <div className='flex w-96 h-22 mx-auto mt-1 justify-center gap-1'>
+        <div className="flex w-96 h-22 mx-auto mt-2 mb-2 justify-center gap-1">
           <button
-            className='w-40 p-1 bg-red-100 border border-red-300 text-xs text-slate-800'
+            className="w-40 p-1 bg-red-100 border border-red-300 text-xs text-slate-800"
             onClick={openModal}
           >
             excluir todos
           </button>
           <button
-            className='w-40 p-1 bg-emerald-50 border grid place-items-center text-green-600 border-green-400'
+            className="w-40 p-1 bg-emerald-50 border grid place-items-center text-green-600 border-green-400"
             onClick={hiddenButtons}
           >
             <AiOutlineCheck />
           </button>
         </div>
       ) : (
-        <div className='flex w-96 h-22 mx-auto mt-2 mb-2 items-center justify-between px-3'>
+        <div className="flex w-96 h-22 mx-auto mt-2 mb-2 items-center justify-between px-3">
           <div>
             <div>
               {canEdit ? (
                 <span
-                  className='text-sm text-slate-700 font-bold border border-solid border-green-400 p-1 cursor-pointer bg-green-50 rounded-md'
-                  onClick={handleGetAllBetHistory}
+                  className="text-sm text-slate-700 font-bold border border-solid border-green-400 p-1 cursor-pointer bg-green-50 rounded-md"
+                  onClick={cancelActionEdit}
                 >
                   cancelar
                 </span>
               ) : (
                 <span
-                  className='text-sm text-slate-700 font-bold border border-solid border-green-400 p-1 cursor-pointer bg-green-50 rounded-md'
+                  className="text-sm text-slate-700 font-bold border border-solid border-green-400 p-1 cursor-pointer bg-green-50 rounded-md"
                   onClick={handleGetAllBetHistory}
                 >
                   {contentButton}
                 </span>
               )}
-              {showButtonEdit && !canEdit && (
+              {showButtonEdit && !canEdit && !isNewBet && (
                 <span
-                  className='text-sm text-slate-700 font-bold border border-solid border-green-400 ml-1 p-1 cursor-pointer bg-green-50 rounded-md'
+                  className="text-sm text-slate-700 font-bold border border-solid border-green-400 ml-1 p-1 cursor-pointer bg-green-50 rounded-md"
                   onClick={permitEditBet}
                 >
                   editar
@@ -157,15 +180,23 @@ export function CheckResult() {
             </div>
           </div>
           {!showAllBets && (
-            <IoOptions
-              className='text-2xl text-slate-700 cursor-pointer'
-              onClick={handleShowButtons}
-            />
+            <div className="flex items-center gap-2">
+              <span
+                className="p-1 bg-emerald-50 border border-emerald-300 rounded-lg text-emerald-700 cursor-pointer"
+                onClick={prepareToCreateNewBet}
+              >
+                <BiPlus />
+              </span>
+              <IoOptions
+                className="text-2xl text-slate-700 cursor-pointer"
+                onClick={handleShowButtons}
+              />
+            </div>
           )}
         </div>
       )}
-      <div className='w-full h-full mx-auto overflow-x-none overflow-y-auto py-4'>
-        <div className='flex w-96 mx-auto flex-col items-center gap-4 py-2 px-12'>
+      <div className="w-full h-full mx-auto overflow-x-none overflow-y-auto py-4">
+        <div className="flex w-96 mx-auto flex-col items-center gap-4 py-2 px-12">
           {showAllBets ? (
             allBetsSave?.map((bet) => (
               <CardBets
@@ -178,22 +209,22 @@ export function CheckResult() {
               />
             ))
           ) : isEmpty ? (
-            <>
-              <ImFilesEmpty className='text-3xl text-slate-700' />
-              <p className='uppercase text-sm text-slate-700'>
+            <div className="w-full mt-14 flex flex-col items-center gap-2 h-full">
+              <ImFilesEmpty className="text-3xl text-slate-700" />
+              <p className="uppercase text-xs text-slate-700">
                 Sem jogos ainda!
               </p>
-            </>
+            </div>
           ) : (
             allBets.map((bet, index) => (
-              <div key={`${index}-${bet}`} className='flex items-center gap-2'>
-                <span className='p-2 bg-slate-50 text-xl font-normal'>
+              <div key={`${index}-${bet}`} className="flex items-center gap-2">
+                <span className="p-2 bg-slate-50 text-xl font-normal">
                   {formatViewBet(bet)}
                 </span>
                 {showSingleButtonDelete && (
-                  <span className='p-3 rounded-lg bg-red-50'>
+                  <span className="p-3 rounded-lg bg-red-50">
                     <CgTrash
-                      className='text-xl cursor-pointer text-red-500'
+                      className="text-xl cursor-pointer text-red-500"
                       onClick={() => removeSomeBet(bet)}
                     />
                   </span>
@@ -204,7 +235,7 @@ export function CheckResult() {
         </div>
       </div>
       {!showAllBets && (
-        <div className='grid place-items-center w-full h-24 pb-2'>
+        <div className="grid place-items-center w-full h-24 pb-2">
           {canEdit ? (
             <button
               className={`w-80 h-12 border border-emerald-600 bg-gradient-to-l from-emerald-600 to-emerald-400 font-bold  ${
@@ -212,15 +243,25 @@ export function CheckResult() {
               } rounded uppercase text-sm text-slate-50`}
               onClick={handleUpdateBets}
             >
-              salvar jogos
+              atualizar jogos
             </button>
           ) : (
-            <button className='w-80 h-12 bg-gradient-to-l from-slate-100 to-slate-50 border border-emerald-300 rounded uppercase text-sm font-bold text-slate-600'>
-              conferir jogos
-            </button>
+            !isEmpty &&
+            (isNewBet ? (
+              <button
+                className="w-80 h-12 border border-emerald-600 bg-gradient-to-l from-emerald-600 to-emerald-400 font-bold uppercase text-sm text-slate-50"
+                onClick={handleSaveBets}
+              >
+                salvar jogos
+              </button>
+            ) : (
+              <button className="w-80 h-12 bg-gradient-to-l from-slate-100 to-slate-50 border border-emerald-300 rounded uppercase text-sm font-bold text-slate-600">
+                conferir jogos
+              </button>
+            ))
           )}
         </div>
       )}
     </div>
-  );
+  )
 }
--- src/context/BetsContext.tsx
+++ src/context/BetsContext.tsx
@@ -1,114 +1,126 @@
-import { createContext, ReactNode, useState } from "react";
-import { ModalAction } from "../components/modalAction";
-import { api } from "../service/api";
+import { createContext, ReactNode, useState } from "react"
+import { ModalAction } from "../components/modalAction"
+import { api } from "../service/api"
 
 interface SaveBetsProps {
-  created_at: string;
-  id_bet: string;
-  my_bets: string[];
+  created_at: string
+  id_bet: string
+  my_bets: string[]
 }
 
 interface BetsResultsProps {
   bet_result: {
-    result: string;
-    date: string;
-  };
-  created_at: string;
-  dataRefId: string;
-  id_user: string;
-  my_bets: string[];
+    result: string
+    date: string
+  }
+  created_at: string
+  dataRefId: string
+  id_user: string
+  my_bets: string[]
 }
 
 interface ResponseBetsProp {
-  status: number;
-  id: string;
+  status: number
+  id: string
 }
 
 interface BetsProps {
-  allBets: number[][];
-  insertBet: (arr: number[]) => void;
-  removeSomeBet: (arr: number[]) => void;
-  replaceBetsWithNewOnes: (newBets: string[]) => void;
-  cleanAllBets: () => void;
-  handleShowBetsHistory: () => void;
-  showBetsHistory: boolean;
-  getAllBetsHistory: (userId: string) => void;
-  allBetsSave: SaveBetsProps[];
-  createAndSaveBets: (arrNumber: number[][], userId: string) => void;
-  updateBets: (bets: number[][], betRef: string) => void;
+  allBets: number[][]
+  insertBet: (arr: number[]) => void
+  removeSomeBet: (arr: number[]) => void
+  replaceBetsWithNewOnes: (newBets: string[]) => void
+  cleanAllBets: () => void
+  handleShowBetsHistory: () => void
+  showBetsHistory: boolean
+  getAllBetsHistory: (userId: string) => void
+  allBetsSave: SaveBetsProps[]
+  createAndSaveBets: (arrNumber: number[][], userId: string) => void
+  updateBets: (bets: number[][], betRef: string) => void
+  permitInsertBets: () => void
+  blockInsertBets: () => void
 }
 
 interface ChildrenProps {
-  children: ReactNode;
+  children: ReactNode
 }
 
-export const BetsContext = createContext({} as BetsProps);
+export const BetsContext = createContext({} as BetsProps)
 
 export function BetsProvider({ children }: ChildrenProps) {
-  const [allBets, setAllBets] = useState<number[][]>([]);
-  const [allBetsSave, setAllBetsSave] = useState<SaveBetsProps[]>();
-  const [showBetsHistory, setShowBetsHistory] = useState<boolean>(false);
-  const [messageModalAction, setMessageModalAction] = useState<string>("");
-  const [showModalAction, setShowModalAction] = useState<boolean>(false);
+  const [allBets, setAllBets] = useState<number[][]>([])
+  const [allBetsSave, setAllBetsSave] = useState<SaveBetsProps[]>()
+  const [showBetsHistory, setShowBetsHistory] = useState<boolean>(false)
+  const [messageModalAction, setMessageModalAction] = useState<string>("")
+  const [showModalAction, setShowModalAction] = useState<boolean>(false)
+  const [canInsertBets, setCanInsertBets] = useState<boolean>(true)
 
-  let bets = [];
+  let bets = []
 
   const insertBet = (arrayBet: number[]) => {
-    bets = arrayBet;
+    if (!canInsertBets) return
+    bets = arrayBet
 
-    setAllBets((oldBets) => [...oldBets, bets]);
-  };
+    setAllBets((oldBets) => [...oldBets, bets])
+  }
+
+  const blockInsertBets = () => {
+    setCanInsertBets(false)
+  }
+
+  const permitInsertBets = () => {
+    setCanInsertBets(true)
+  }
 
   const replaceBetsWithNewOnes = (bets: string[]) => {
-    const myBets = transformArrStringToNumber(bets);
-    setAllBets(myBets);
-  };
+    const myBets = transformArrStringToNumber(bets)
+    setAllBets(myBets)
+  }
 
   const removeSomeBet = (arrayBet: number[]) => {
     const betsAfterRemove = allBets.filter((bet) => {
-      return bet !== arrayBet;
-    });
+      return bet !== arrayBet
+    })
 
-    setAllBets(betsAfterRemove);
-  };
+    setAllBets(betsAfterRemove)
+  }
 
   const cleanAllBets = () => {
-    setAllBets([]);
-  };
+    setAllBets([])
+  }
 
   const handleShowBetsHistory = () => {
-    setShowBetsHistory((oldState) => !oldState);
-  };
+    setShowBetsHistory((oldState) => !oldState)
+  }
 
   const getAllBetsHistory = async (userId: string) => {
-    handleShowBetsHistory();
+    handleShowBetsHistory()
 
     if (!showBetsHistory) {
-      const allBetsData = await api.get("/bets", { params: { userId } });
-      const dataResults = [];
+      const allBetsData = await api.get("/bets", { params: { userId } })
+      const dataResults = []
 
       allBetsData.data.forEach((bet: BetsResultsProps) => {
         const data = {
           created_at: bet.created_at,
           id_bet: bet.dataRefId,
           my_bets: bet.my_bets,
-        };
-        dataResults.push(data);
-      });
+        }
+        dataResults.push(data)
+      })
 
-      setAllBetsSave(dataResults);
+      setAllBetsSave(dataResults)
     }
-  };
+  }
 
   const transformArrStringToNumber = (bets: string[]) => {
     const numberArr = bets.map((bet) => {
-      const betToNumber = bet.split("-").map((item) => Number(item.trim()));
+      const betToNumber = bet.split("-").map((item) => Number(item.trim()))
 
-      return betToNumber;
-    });
+      return betToNumber
+    })
 
-    return numberArr;
-  };
+    return numberArr
+  }
 
   const transformArrNumberToString = (arrNumbers: number[][]) => {
     const arrString = arrNumbers
@@ -116,66 +128,63 @@ export function BetsProvider({ children }: ChildrenProps) {
         return arrNumber.reduce(
           (acc, number) => acc + String(number) + " - ",
           ""
-        );
+        )
       })
-      .map((bet) => bet.substring(0, [...bet].length - 3));
+      .map((bet) => bet.substring(0, [...bet].length - 3))
 
-    return arrString;
-  };
+    return arrString
+  }
 
   const updateBets = async (bets: number[][], betRef: string) => {
-    const result = transformArrNumberToString(bets);
+    const result = transformArrNumberToString(bets)
 
-    console.log(result, betRef);
     try {
       const { status } = await api.patch("bets", {
         data: result,
         betRef,
-      });
+      })
 
       if (status === 204) {
-        setMessageModalAction("Aposta atualizada com sucesso!");
-        setShowModalAction(true);
+        setMessageModalAction("Aposta atualizada com sucesso!")
+        setShowModalAction(true)
 
         setTimeout(() => {
-          setShowModalAction(false);
-        }, 1500);
+          setShowModalAction(false)
+        }, 1500)
       }
-
-      console.log(status);
     } catch (err) {
-      console.log(err);
+      console.log(err)
     }
-  };
+  }
 
   const createAndSaveBets = async (bets: number[][], userId: string) => {
-    const result = transformArrNumberToString(bets);
+    const result = transformArrNumberToString(bets)
 
     try {
       const data = {
         created_at: new Date().toISOString(),
         id_user: userId,
         my_bets: result,
-      };
+      }
 
       const {
         status,
         data: { id },
-      } = await api.post("bets", data);
+      } = await api.post("bets", data)
 
       if (status === 200) {
-        setMessageModalAction("Aposta salva com sucesso!");
-        setShowModalAction(true);
+        setMessageModalAction("Aposta salva com sucesso!")
+        setShowModalAction(true)
 
         setTimeout(() => {
-          setShowModalAction(false);
-        }, 1500);
+          setShowModalAction(false)
+        }, 1500)
       }
     } catch (err) {
-      console.log(err);
-      alert(err);
+      console.log(err)
+      alert(err)
     }
-  };
+  }
 
   return (
     <BetsContext.Provider
@@ -191,10 +200,12 @@ export function BetsProvider({ children }: ChildrenProps) {
         allBetsSave,
         createAndSaveBets,
         updateBets,
+        blockInsertBets,
+        permitInsertBets,
       }}
     >
       {children}
       {showModalAction && <ModalAction message={messageModalAction} />}
     </BetsContext.Provider>
-  );
+  )
 }
